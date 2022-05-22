d025945280d042e345ffc1ef2ff831511e354632
reden96 <23635491+reden96@users.noreply.github.com>
2022-05-22 12:46:25 +0100

    Add project files.

---
+++ SVC/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace SVC
+{
+    partial class svcWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(svcWindow));
+            this.ActivateButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ActivateButton
+            // 
+            this.ActivateButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ActivateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ActivateButton.Location = new System.Drawing.Point(0, 304);
+            this.ActivateButton.Name = "ActivateButton";
+            this.ActivateButton.Size = new System.Drawing.Size(800, 76);
+            this.ActivateButton.TabIndex = 0;
+            this.ActivateButton.Text = "Stop voice commands";
+            this.ActivateButton.UseVisualStyleBackColor = true;
+            this.ActivateButton.Click += new System.EventHandler(this.activateButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 93);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open list of currently available voice commands";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // svcWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 380);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ActivateButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "svcWindow";
+            this.Text = "Steam Voice Commands";
+            this.Load += new System.EventHandler(this.svcWindow_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.svcWindow_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ActivateButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
