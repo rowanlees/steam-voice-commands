13592b7584c4192198cab7d0db99887768002fc4
Rowan Lees <23635491+rowanlees@users.noreply.github.com>
2025-07-20 00:10:13 +0100

    Tidy

--- SVC/src/Application.cs
+++ SVC/src/Application.cs
@@ -14,14 +14,14 @@ namespace SVC
         static void Main()
         {
             var fileSystem = new FileSystem();
-            GameLocationsService gameLocations = new GameLocationsService(
+            GameLocationsService gameLocationsService = new GameLocationsService(
                 new GameManifestParser(),
                 //10 second timeout looking for steam installation directory
                 new SteamInstallationLocator(new ProcessWrapper(new System.Diagnostics.Process()), 10000),
                 new SteamLibraryReader(fileSystem),
                 new GameRepository(fileSystem)
             );
-            gameLocations.BuildGameList();
+            gameLocationsService.BuildGameList();
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             System.Windows.Forms.Application.Run(new SvcWindow());
