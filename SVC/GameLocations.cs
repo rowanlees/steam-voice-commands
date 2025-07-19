f888ddfec6166105fa42362cc480f9d59cdce445
Rowan Lees <23635491+rowanlees@users.noreply.github.com>
2025-07-19 17:40:15 +0100

    Tidy

--- SVC/GameLocations.cs
+++ SVC/GameLocations.cs
@@ -8,12 +8,13 @@ namespace SVC
     internal class GameLocations
     {
         public const string GamesListFileName = "gameslist.txt";
-        string steamExeLocation;
-        string steamFolderLocation;
-        private readonly string currentDirectory = Directory.GetCurrentDirectory();
+        private const string SteamInstallLocationFileName = "steaminstalllocation.txt";
+        private string _steamExeLocation;
+        private string _steamFolderLocation;
+        private readonly string _currentDirectory = Directory.GetCurrentDirectory();
         private bool _fileExists = false;
         private readonly string _steamInstallRegQuery = "cmd /c REG QUERY HKCU\\SOFTWARE\\Valve\\Steam /f SteamExe >steaminstalllocation.txt";
-        private Dictionary<string, string> _gamesList = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> _gamesList = new Dictionary<string, string>();
         private readonly List<string> libraryFolders = new List<string>();
 
         public void QuerySteamInstallLocation()
@@ -21,13 +22,13 @@ namespace SVC
             Process process = new Process();
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.WorkingDirectory = currentDirectory;
+            process.StartInfo.WorkingDirectory = _currentDirectory;
             process.StartInfo.FileName = "cmd.exe";
             process.StartInfo.Arguments = _steamInstallRegQuery;
             process.Start();
             while (_fileExists == false)
             {
-                if (File.Exists(currentDirectory + "\\steaminstalllocation.txt"))
+                if (File.Exists(_currentDirectory + Path.DirectorySeparatorChar + SteamInstallLocationFileName))
                 {
                     _fileExists = true;
                     process.WaitForExit();
@@ -40,17 +41,17 @@ namespace SVC
 
         private void WriteSteamInstallLocation()
         {
-            steamExeLocation = File.ReadAllText(currentDirectory + "\\steaminstalllocation.txt");
-            steamExeLocation = steamExeLocation.TextAfter("SZ");
-            steamExeLocation = steamExeLocation.GetUntilOrEmpty("End of search");
-            steamExeLocation = steamExeLocation.Trim();
-            steamFolderLocation = steamExeLocation.GetUntilOrEmpty("/steam.exe");
-            File.WriteAllText(currentDirectory + "\\steamfolderlocation.txt", steamFolderLocation);
+            _steamExeLocation = File.ReadAllText(_currentDirectory + Path.DirectorySeparatorChar + SteamInstallLocationFileName);
+            _steamExeLocation = _steamExeLocation.TextAfter("SZ");
+            _steamExeLocation = _steamExeLocation.GetUntilOrEmpty("End of search");
+            _steamExeLocation = _steamExeLocation.Trim();
+            _steamFolderLocation = _steamExeLocation.GetUntilOrEmpty("/steam.exe");
+            File.WriteAllText(_currentDirectory + "\\steamfolderlocation.txt", _steamFolderLocation);
         }
 
         private void ReadLibraryFolders()
         {
-            var lines = File.ReadLines(steamFolderLocation + "/steamapps/libraryfolders.vdf");
+            var lines = File.ReadLines(_steamFolderLocation + "/steamapps/libraryfolders.vdf");
             foreach (string line in lines)
             {
                 if (line.Contains("path"))
@@ -63,7 +64,7 @@ namespace SVC
                 }
             }
             string libraryFoldersCombined = string.Join(",", libraryFolders);
-            File.WriteAllText(currentDirectory + "\\steamlibraryfolders.txt", libraryFoldersCombined);
+            File.WriteAllText(_currentDirectory + "\\steamlibraryfolders.txt", libraryFoldersCombined);
         }
 
         private void ReadManifestFiles()
@@ -90,7 +91,7 @@ namespace SVC
                     _gamesList.Add(gameName, appid);
                 }
             }
-            using (StreamWriter file = new StreamWriter(currentDirectory + Path.DirectorySeparatorChar + GamesListFileName))
+            using (StreamWriter file = new StreamWriter(_currentDirectory + Path.DirectorySeparatorChar + GamesListFileName))
                 foreach (var entry in _gamesList)
                     file.WriteLine("{0}\n{1}", entry.Key, entry.Value);
         }
